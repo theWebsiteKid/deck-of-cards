a130becc5d03b0f8c2da747866a6d5d5f8569281
Xavier Duncan <xduncan@pm.me>
2018-10-02 18:56:13 -0400

    init app skeleton

---
+++ App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>
+          Hello World ⚡️
+        </Text>
+      </View>
+    )
+  };
+};
+
+export default App;
